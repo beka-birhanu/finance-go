0eab1ec82437da9382142b9fa368de775c082ce6
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 01:05:01 +0300

    feat(utils): add json encoding ena decoding

---
+++ api/utils/utils.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var Validate = validator.New()
+
+func ParseJSON(r *http.Request, v any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
+}
+
