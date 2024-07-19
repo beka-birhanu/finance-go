7d8a05d17256c480e189bebad571959648c8eb25
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 01:08:06 +0300

    feat(repository): implement user repo using inmemory storage

--- infrastructure/repositories/user_repository.go
+++ infrastructure/repositories/user_repository.go
@@ -2,14 +2,19 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
 	"github.com/beka-birhanu/finance-go/domain/entities"
+	"github.com/google/uuid"
 )
 
 type UserRepository struct {
 	DB *sql.DB
 }
+
+var users = map[uuid.UUID]entities.User{}
 
 // Ensure UserRepository implements interfaces.persistance.IUserRepository
 var _ persistance.IUserRepository = &UserRepository{}
@@ -20,80 +25,27 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 	}
 }
 
-// CreateExpense implements persistance.IUserRepository.
-func (u *UserRepository) CreateExpense(expense *entities.User) error {
+func (u *UserRepository) CreateUser(user *entities.User) error {
+	users[user.ID] = *user
+	log.Println("userCreated")
+
+	return nil
+}
+
+func (u *UserRepository) GetUserById(id string) (*entities.User, error) {
 	panic("unimplemented")
 }
 
-// GetExpense implements persistance.IUserRepository.
-func (u *UserRepository) GetExpense(id string) (*entities.User, error) {
-	panic("unimplemented")
+func (u *UserRepository) GetUserByUsername(username string) (*entities.User, error) {
+	for _, user := range users {
+		if user.Username == username {
+			return &user, nil
+		}
+	}
+
+	return nil, fmt.Errorf("not found")
 }
 
-// ListExpenses implements persistance.IUserRepository.
-func (u *UserRepository) ListExpenses() ([]*entities.User, error) {
+func (u *UserRepository) ListUser() ([]*entities.User, error) {
 	panic("unimplemented")
 }
-
-// package repositories
-//
-// import (
-// 	"errors"
-// 	"sync"
-//
-// 	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
-// 	"github.com/beka-birhanu/finance-go/domain/entities"
-// )
-//
-// type InMemoryUserRepository struct {
-// 	mu    sync.RWMutex
-// 	users map[string]*entities.User
-// }
-//
-// // Ensure InMemoryUserRepository implements persistance.IUserRepository
-// var _ persistance.IUserRepository = &InMemoryUserRepository{}
-//
-// func NewInMemoryUserRepository() *InMemoryUserRepository {
-// 	return &InMemoryUserRepository{
-// 		users: make(map[string]*entities.User),
-// 	}
-// }
-//
-// // CreateUser implements persistance.IUserRepository
-// func (r *InMemoryUserRepository) CreateUser(user *entities.User) error {
-// 	r.mu.Lock()
-// 	defer r.mu.Unlock()
-//
-// 	if _, exists := r.users[user.ID]; exists {
-// 		return errors.New("user already exists")
-// 	}
-//
-// 	r.users[user.ID] = user
-// 	return nil
-// }
-//
-// // GetUser implements persistance.IUserRepository
-// func (r *InMemoryUserRepository) GetUser(id string) (*entities.User, error) {
-// 	r.mu.RLock()
-// 	defer r.mu.RUnlock()
-//
-// 	user, exists := r.users[id]
-// 	if !exists {
-// 		return nil, errors.New("user not found")
-// 	}
-//
-// 	return user, nil
-// }
-//
-// // ListUsers implements persistance.IUserRepository
-// func (r *InMemoryUserRepository) ListUsers() ([]*entities.User, error) {
-// 	r.mu.RLock()
-// 	defer r.mu.RUnlock()
-//
-// 	var users []*entities.User
-// 	for _, user := range r.users {
-// 		users = append(users, user)
-// 	}
-//
-// 	return users, nil
-// }
