88455981985e4c917247c9dd4d3fb786295dce8f
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-19 18:50:12 +0300

    feat(api): add api service

--- api/api.go
+++ api/api.go
@@ -1,1 +1,39 @@
 package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/beka-birhanu/finance-go/api/users"
+	"github.com/beka-birhanu/finance-go/infrastructure/repositories"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userRepository := repositories.NewUserRepository(s.db)
+	userHandler := users.NewHandler(userRepository)
+	userHandler.RegisterRoutes(subrouter)
+
+	// Serve static files
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
