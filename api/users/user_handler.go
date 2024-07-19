2423867b1728908467923e9e0d3bf60c1ec897f5
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 01:12:37 +0300

    feat(user-handler): consume auth command and handler for register, close #1

--- api/users/user_handler.go
+++ api/users/user_handler.go
@@ -1,19 +1,26 @@
 package users
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
-	"github.com/beka-birhanu/finance-go/infrastructure/repositories"
+	"github.com/beka-birhanu/finance-go/api/users/dto"
+	"github.com/beka-birhanu/finance-go/api/utils"
+	"github.com/beka-birhanu/finance-go/application/authentication/commands"
+	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
-	UserRepository *repositories.UserRepository
+	UserRepository             persistance.IUserRepository
+	UserRegisterCommandHandler *commands.UserRegisterCommandHandler
 }
 
-func NewHandler(userRepository *repositories.UserRepository) *Handler {
-	return &Handler{UserRepository: userRepository}
+func NewHandler(userRepository persistance.IUserRepository) *Handler {
+	commandHandler := commands.NewRegisterCommandHandler(userRepository)
+
+	return &Handler{UserRepository: userRepository, UserRegisterCommandHandler: commandHandler}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -24,5 +31,33 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) handleUserRegisteration(w http.ResponseWriter, r *http.Request) {
-	log.Println("working")
+	var registerRequest dto.RegisterRequest
+
+	if err := utils.ParseJSON(r, &registerRequest); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(registerRequest); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	registerCommand, err := commands.NewUserRegisterCommand(registerRequest.Username, registerRequest.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := h.UserRegisterCommandHandler.Handle(registerCommand); err != nil {
+		if err == commands.ErrUsernameInUse {
+			utils.WriteError(w, http.StatusConflict, err)
+		} else {
+			utils.WriteError(w, http.StatusInternalServerError, err)
+		}
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, map[string]string{"token": "token"})
 }
