9c6e8c92ac402341900160e62b14318cd0ae47b0
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 19:48:57 +0300

    fix(auth-query): use hash service to compare password

--- application/authentication/queries/user_login_query_handler.go
+++ application/authentication/queries/user_login_query_handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/beka-birhanu/finance-go/application/authentication/common"
+	"github.com/beka-birhanu/finance-go/application/common/interfaces/hash"
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/jwt"
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
 )
@@ -11,10 +12,11 @@ import (
 type UserLoginQueryHandler struct {
 	UserRepository persistance.IUserRepository
 	JwtService     jwt.IJwtService
+	HashService    hash.IHashService
 }
 
-func NewUserLoginQueryHandler(repository persistance.IUserRepository, jwtService jwt.IJwtService) *UserLoginQueryHandler {
-	return &UserLoginQueryHandler{UserRepository: repository, JwtService: jwtService}
+func NewUserLoginQueryHandler(repository persistance.IUserRepository, jwtService jwt.IJwtService, hashService hash.IHashService) *UserLoginQueryHandler {
+	return &UserLoginQueryHandler{UserRepository: repository, JwtService: jwtService, HashService: hashService}
 }
 
 func (h *UserLoginQueryHandler) Handle(query *UserLoginQuery) (*common.AuthResult, error) {
@@ -23,7 +25,12 @@ func (h *UserLoginQueryHandler) Handle(query *UserLoginQuery) (*common.AuthResul
 		return nil, fmt.Errorf("invalid username or password")
 	}
 
-	if user.Password != query.Password {
+	isPassowrdCorrect, err := h.HashService.Match(user.Password, query.Password)
+	if err != nil {
+		return nil, fmt.Errorf("server error")
+	}
+
+	if !isPassowrdCorrect {
 		return nil, fmt.Errorf("invalid username or password")
 	}
 
