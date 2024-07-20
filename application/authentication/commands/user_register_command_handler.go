3bfb2b343bccfb3b0d2fa1f4ba2e60edba7f7703
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 19:48:30 +0300

    fix(auth-command): hash password before storing

--- application/authentication/commands/user_register_command_handler.go
+++ application/authentication/commands/user_register_command_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/beka-birhanu/finance-go/application/authentication/common"
+	"github.com/beka-birhanu/finance-go/application/common/interfaces/hash"
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/jwt"
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
 	"github.com/beka-birhanu/finance-go/domain/entities"
@@ -13,14 +14,16 @@ import (
 type UserRegisterCommandHandler struct {
 	UserRepository persistance.IUserRepository
 	JwtService     jwt.IJwtService
+	HashService    hash.IHashService
 }
 
 var (
 	ErrUsernameInUse = errors.New("username in use")
 )
 
-func NewRegisterCommandHandler(repository persistance.IUserRepository, jwtService jwt.IJwtService) *UserRegisterCommandHandler {
-	return &UserRegisterCommandHandler{UserRepository: repository, JwtService: jwtService}
+func NewRegisterCommandHandler(repository persistance.IUserRepository, jwtService jwt.IJwtService, hashService hash.IHashService) *UserRegisterCommandHandler {
+
+	return &UserRegisterCommandHandler{UserRepository: repository, JwtService: jwtService, HashService: hashService}
 }
 
 func (h *UserRegisterCommandHandler) Handle(command *UserRegisterCommand) (*common.AuthResult, error) {
@@ -28,7 +31,11 @@ func (h *UserRegisterCommandHandler) Handle(command *UserRegisterCommand) (*comm
 		return nil, ErrUsernameInUse
 	}
 
-	user := fromRegisterCommand(command)
+	user, err := fromRegisterCommand(command, h.HashService)
+	if err != nil {
+		return nil, fmt.Errorf("server error")
+	}
+
 	if err := h.UserRepository.CreateUser(user); err != nil {
 		return nil, fmt.Errorf("server error")
 	}
@@ -41,13 +48,17 @@ func (h *UserRegisterCommandHandler) Handle(command *UserRegisterCommand) (*comm
 	return common.NewAuthResult(user.ID, user.Username, token), nil
 }
 
-func fromRegisterCommand(command *UserRegisterCommand) *entities.User {
+func fromRegisterCommand(command *UserRegisterCommand, hashService hash.IHashService) (*entities.User, error) {
 	var user entities.User
 
 	user.ID = command.ID
 	user.Username = command.Username
-	// TODO: hash
-	user.Password = command.Password
 
-	return &user
+	hashedPassword, err := hashService.Hash(command.Password)
+	if err != nil {
+		return nil, fmt.Errorf("server error")
+	}
+	user.Password = hashedPassword
+
+	return &user, nil
 }
