e8125311d28f0f830dec93317e6d8954b66e7ebd
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 19:45:07 +0300

    feat(infrastructure-hash): implement hash service

---
+++ infrastructure/hash/hash_service.go
@@ -0,0 +1,70 @@
+package hash
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"log"
+	"sync"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+const (
+	iterations = 10000
+	saltSize   = 16
+	keySize    = 32
+)
+
+type HashService struct{}
+
+var (
+	instance *HashService
+	once     sync.Once
+)
+
+func GetHashService() *HashService {
+	once.Do(func() {
+		instance = &HashService{}
+	})
+	return instance
+}
+
+func (hs *HashService) Hash(word string) (string, error) {
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	hash := pbkdf2.Key([]byte(word), salt, iterations, keySize, sha256.New)
+
+	result := append(salt, hash...)
+
+	log.Println(base64.StdEncoding.EncodeToString(result))
+	return base64.StdEncoding.EncodeToString(result), nil
+}
+
+func (hs *HashService) Match(hashedWord, plainWord string) (bool, error) {
+	hashedWordBytes, err := base64.StdEncoding.DecodeString(hashedWord)
+	if err != nil {
+		return false, err
+	}
+
+	if len(hashedWordBytes) != saltSize+keySize {
+		return false, errors.New("invalid hashed word length")
+	}
+
+	salt := hashedWordBytes[:saltSize]
+	expectedHash := hashedWordBytes[saltSize:]
+
+	hash := pbkdf2.Key([]byte(plainWord), salt, iterations, keySize, sha256.New)
+	for i := 0; i < keySize; i++ {
+		if expectedHash[i] != hash[i] {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
