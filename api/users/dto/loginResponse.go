8c8433bcbb4a92417350ea820e54c77ef574db82
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 20:50:23 +0300

    fix(users-dto): remove token from loginResponse

--- api/users/dto/loginResponse.go
+++ api/users/dto/loginResponse.go
@@ -7,9 +7,8 @@ import (
 type LoginResponse struct {
 	ID       string `json:"id"`
 	Username string `json:"username"`
-	Token    string `json:"token"`
 }
 
 func FromAuthResult(authResult *common.AuthResult) *LoginResponse {
-	return &LoginResponse{ID: authResult.ID.String(), Username: authResult.Username, Token: authResult.Token}
+	return &LoginResponse{ID: authResult.ID.String(), Username: authResult.Username}
 }
