a2338c0b6f4774be8f7abeb16c72ed06c73386f3
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 18:36:34 +0300

    feat(infrastructure): implement jwt service

---
+++ infrastructure/jwt/jwt_service.go
@@ -0,0 +1,55 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/beka-birhanu/finance-go/domain/entities"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JwtService struct {
+	secretKey string
+	issuer    string
+	expTime   time.Duration
+}
+
+func NewJwtService(secretKey, issuer string, expTime time.Duration) *JwtService {
+	return &JwtService{
+		secretKey: secretKey,
+		issuer:    issuer,
+		expTime:   expTime,
+	}
+}
+
+func (s *JwtService) GenerateToken(user *entities.User) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": user.ID.String(),
+		"exp":     time.Now().Add(s.expTime).Unix(),
+		"iss":     s.issuer,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(s.secretKey))
+}
+
+func (s *JwtService) DecodeToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Check the signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(s.secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
+
