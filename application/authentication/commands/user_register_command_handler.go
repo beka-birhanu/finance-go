fc11960d88eeee008519f3c37dac336f10fd69eb
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-20 18:40:39 +0300

    feat(auth-commands): consume jwt service to return token in register

--- application/authentication/commands/user_register_command_handler.go
+++ application/authentication/commands/user_register_command_handler.go
@@ -4,33 +4,41 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/beka-birhanu/finance-go/application/authentication/common"
+	"github.com/beka-birhanu/finance-go/application/common/interfaces/jwt"
 	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
 	"github.com/beka-birhanu/finance-go/domain/entities"
 )
 
 type UserRegisterCommandHandler struct {
 	UserRepository persistance.IUserRepository
+	JwtService     jwt.IJwtService
 }
 
 var (
 	ErrUsernameInUse = errors.New("username in use")
 )
 
-func NewRegisterCommandHandler(repository persistance.IUserRepository) *UserRegisterCommandHandler {
-	return &UserRegisterCommandHandler{UserRepository: repository}
+func NewRegisterCommandHandler(repository persistance.IUserRepository, jwtService jwt.IJwtService) *UserRegisterCommandHandler {
+	return &UserRegisterCommandHandler{UserRepository: repository, JwtService: jwtService}
 }
 
-func (h *UserRegisterCommandHandler) Handle(command *UserRegisterCommand) error {
+func (h *UserRegisterCommandHandler) Handle(command *UserRegisterCommand) (*common.AuthResult, error) {
 	if _, err := h.UserRepository.GetUserByUsername(command.Username); err == nil {
-		return ErrUsernameInUse
+		return nil, ErrUsernameInUse
 	}
 
 	user := fromRegisterCommand(command)
 	if err := h.UserRepository.CreateUser(user); err != nil {
-		return fmt.Errorf("server error")
+		return nil, fmt.Errorf("server error")
 	}
 
-	return nil
+	token, err := h.JwtService.GenerateToken(user)
+	if err != nil {
+		return nil, fmt.Errorf("server error")
+	}
+
+	return common.NewAuthResult(user.ID, user.Username, token), nil
 }
 
 func fromRegisterCommand(command *UserRegisterCommand) *entities.User {
