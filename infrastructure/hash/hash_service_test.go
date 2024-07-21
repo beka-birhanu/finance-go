b3173c03a1c4b7afeabb3546549c289d8e3967ec
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-21 16:07:31 +0300

    refactor(hash-test): split match test into correct and incorrect test

--- infrastructure/hash/hash_service_test.go
+++ infrastructure/hash/hash_service_test.go
@@ -2,44 +2,54 @@ package hash
 
 import "testing"
 
-var hashService = GetHashService()
+func TestHashService(t *testing.T) {
+	hashService := GetHashService()
 
-func TestHash(t *testing.T) {
-	hashedWord, err := hashService.Hash("plainWord")
-	if err != nil {
-		t.Errorf("error hashing plainWord: %v", err)
-	}
+	t.Run("HashWord", func(t *testing.T) {
+		hashedWord, err := hashService.Hash("plainWord")
+		if err != nil {
+			t.Errorf("error hashing plainWord: %v", err)
+		}
 
-	if hashedWord == "" {
-		t.Error("expected hash to be not empty")
-	}
+		if hashedWord == "" {
+			t.Error("expected hash to be not empty")
+		}
 
-	if hashedWord == "plainWord" {
-		t.Error("expected hash to be diffrent from plainWord")
-	}
+		if hashedWord == "plainWord" {
+			t.Error("expected hash to be different from plainWord")
+		}
+	})
+
+	t.Run("MatchHashedWithCorrectPlain", func(t *testing.T) {
+		hashedWord, err := hashService.Hash("plainWord")
+		if err != nil {
+			t.Errorf("error hashing plainWord: %v", err)
+		}
+
+		matchResult, err := hashService.Match(hashedWord, "plainWord")
+		if err != nil {
+			t.Errorf("error matching plainWord: %v", err)
+		}
+
+		if !matchResult {
+			t.Errorf("expected password to match hash")
+		}
+	})
+
+	t.Run("MatchHashedWithIncorrectPlain", func(t *testing.T) {
+		hashedWord, err := hashService.Hash("plainWord")
+		if err != nil {
+			t.Errorf("error hashing plainWord: %v", err)
+		}
+
+		matchResult, err := hashService.Match(hashedWord, "incorrectPlainWord")
+		if err != nil {
+			t.Errorf("error matching incorrectPlainWord: %v", err)
+		}
+
+		if matchResult {
+			t.Errorf("expected incorrectPlainWord to not match hash")
+		}
+	})
 }
 
-func TestMatchHash(t *testing.T) {
-	hashedWrod, err := hashService.Hash("plainWord")
-	if err != nil {
-		t.Errorf("error hashing plainWord: %v", err)
-	}
-
-	matchResult, err := hashService.Match(hashedWrod, "plainWord")
-	if err != nil {
-		t.Errorf("error matching plainWord: %v", err)
-	}
-
-	if matchResult != true {
-		t.Errorf("expected password to match hash")
-	}
-
-	matchResult, err = hashService.Match(hashedWrod, "incorrect plainWord")
-	if err != nil {
-		t.Errorf("error matching incorrect plainWord: %v", err)
-	}
-
-	if matchResult == true {
-		t.Errorf("expected incorrect plainWord to not match hash")
-	}
-}
