feb206af6b5ffe84bbf6d51b8a8090efe836bf58
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-22 16:52:18 +0300

    feat(domain-errors): add expense related errors

---
+++ domain/domain_errors/expense_error.go
@@ -0,0 +1,9 @@
+package domain_errors
+
+import "errors"
+
+var (
+	ErrNegativeExpenseAmount     = errors.New("Expense.Amount cannot be negative")
+	ErrExpenseDescriptionTooLong = errors.New("Expense.Description is too long")
+	ErrEmptyExpenseDescription   = errors.New("Expense.Description cannot be empty")
+)
