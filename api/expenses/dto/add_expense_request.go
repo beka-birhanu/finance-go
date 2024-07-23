443d9a8c5268f5043235f0fbe3e0447c6ad5404c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-23 10:48:43 +0300

    fix(expenses-dto): change addExpenseRequest to match design, refs #13

--- api/expenses/dto/add_expense_request.go
+++ api/expenses/dto/add_expense_request.go
@@ -2,11 +2,13 @@ package dto
 
 import (
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type AddExpenseRequest struct {
-	UserId string    `json:"userId" validate:"required"`
-	Title  string    `json:"title" validate:"required"`
-	Amount float32   `json:"amount" validate:"required"`
-	Data   time.Time `json:"date" validate:"required"`
+	UserId      uuid.UUID `json:"userId" validate:"required"`
+	Description string    `json:"description" validate:"required"`
+	Amount      float32   `json:"amount" validate:"required"`
+	Data        time.Time `json:"date" validate:"required"`
 }
