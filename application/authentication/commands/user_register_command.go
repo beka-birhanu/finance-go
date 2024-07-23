ce25342a14c5a7e44c88b90768cc22a2d723a0d7
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-23 10:41:16 +0300

    refactor(auth-commands): remove unused fields form register command

--- application/authentication/commands/user_register_command.go
+++ application/authentication/commands/user_register_command.go
@@ -1,23 +1,10 @@
 package commands
 
-import (
-	"fmt"
-
-	"github.com/google/uuid"
-)
-
 type UserRegisterCommand struct {
-	ID       uuid.UUID
 	Username string
 	Password string
 }
 
 func NewUserRegisterCommand(username, password string) (*UserRegisterCommand, error) {
-	id, err := uuid.NewV7()
-
-	if err != nil {
-		return nil, fmt.Errorf("server error")
-	}
-
-	return &UserRegisterCommand{ID: id, Username: username, Password: password}, nil
+	return &UserRegisterCommand{Username: username, Password: password}, nil
 }
