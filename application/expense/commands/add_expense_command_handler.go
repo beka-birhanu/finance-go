bc3523fe95b3c684d84cc2ea6752566eb09b1f50
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-23 13:19:41 +0300

    feat(app-expense): add AddExpenseCommandHandler, refs #13

---
+++ application/expense/commands/add_expense_command_handler.go
@@ -0,0 +1,44 @@
+package commands
+
+import (
+	"github.com/beka-birhanu/finance-go/application/common/interfaces/persistance"
+	"github.com/beka-birhanu/finance-go/domain/models"
+)
+
+type AddExpenseCommandHandler struct {
+	userRepository persistance.IUserRepository
+}
+
+func NewAddExpenseCommandHandler(userRepository persistance.IUserRepository) *AddExpenseCommandHandler {
+	return &AddExpenseCommandHandler{userRepository: userRepository}
+}
+
+func (h *AddExpenseCommandHandler) Handle(command *AddExpenseCommand) (*models.Expense, error) {
+	newExpense, err := fromAddExpenseCommand(command)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := h.userRepository.GetUserById(command.UserId.String())
+	if err != nil {
+		return nil, err
+	}
+
+	err = user.AddExpense(newExpense)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: save user
+
+	return newExpense, nil
+}
+
+func fromAddExpenseCommand(command *AddExpenseCommand) (*models.Expense, error) {
+	return models.NewExpense(
+		command.Description,
+		command.Amount,
+		command.UserId,
+		command.Date,
+	)
+}
