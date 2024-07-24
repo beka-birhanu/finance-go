203f432680e726531b7600c0be3d0626ec3507a4
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 01:34:18 +0300

    docs(app-command): add godocs to addExpenseCommand, refs #14

--- application/expense/command/add_expense_command.go
+++ application/expense/command/add_expense_command.go
@@ -6,9 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
+// AddExpenseCommand represents the command to add an expense.
 type AddExpenseCommand struct {
-	UserId      uuid.UUID
-	Date        time.Time
+	// UserId: The unique identifier of the user to whom the expense belongs.
+	UserId uuid.UUID
+
+	// Date: The date when the expense occurred.
+	Date time.Time
+
+	// Description: A brief description of the expense.
 	Description string
-	Amount      float32
+
+	// Amount: The amount of the expense. Must be a positive float value.
+	Amount float32
 }
