346dba3aa34421066df309d9d5a8391cadcd17f1
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 00:53:54 +0300

    docs(app-interface): add godoc for ITimeService, refs #14

--- application/common/interface/time_service/time_service.go
+++ application/common/interface/time_service/time_service.go
@@ -1,7 +1,17 @@
+/*
+Package timeservice provides an interface for time-related operations.
+
+It includes the `ITimeService` interface for obtaining the current UTC time.
+*/
 package timeservice
 
 import "time"
 
+// ITimeService defines methods for obtaining time information.
+//
+// Methods:
+// - NowUTC() time.Time: Returns the current time in UTC.
 type ITimeService interface {
+	// NowUTC returns the current time in UTC.
 	NowUTC() time.Time
 }
