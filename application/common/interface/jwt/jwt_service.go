0ecf385283f56e8def82f52485730ec8ff77524a
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 00:52:39 +0300

    docs(app-interface): add godoc for IJwtService, refs #14

--- application/common/interface/jwt/jwt_service.go
+++ application/common/interface/jwt/jwt_service.go
@@ -1,11 +1,26 @@
+/*
+Package jwt provides an interface for handling JSON Web Tokens (JWT).
+
+It includes the `IJwtService` interface for generating and decoding JWTs.
+*/
 package jwt
 
 import (
-	"github.com/beka-birhanu/finance-go/domain/model"
+	"github.com/beka-birhanu/finance-go/domain/model/user"
 	"github.com/dgrijalva/jwt-go"
 )
 
+// IJwtService defines methods for generating and decoding JSON Web Tokens (JWT).
+//
+// Methods:
+// - GenerateToken(user *usermodel.User) (string, error): Generates a JWT for the
+// given user and returns the token or an error.
+// - DecodeToken(token string) (jwt.MapClaims, error): Decodes the provided JWT
+// and returns the claims or an error.
 type IJwtService interface {
-	GenerateToken(user *model.User) (string, error)
+	// GenerateToken creates a JWT for the specified user.
+	GenerateToken(user *usermodel.User) (string, error)
+
+	// DecodeToken parses the provided JWT and returns the claims or an error.
 	DecodeToken(token string) (jwt.MapClaims, error)
 }
