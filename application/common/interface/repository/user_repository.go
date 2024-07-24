599ca9502ea4dd8ce4eb6670fcd4266e67e05971
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 00:53:29 +0300

    docs(app-interface): add godoc for IUserRepository, refs#14

--- application/common/interface/repository/user_repository.go
+++ application/common/interface/repository/user_repository.go
@@ -1,13 +1,37 @@
+/*
+Package repository provides an interface for user data access operations.
+
+It includes the `IUserRepository` interface for managing user data in a repository.
+*/
 package repository
 
 import (
-	"github.com/beka-birhanu/finance-go/domain/model"
+	"github.com/beka-birhanu/finance-go/domain/model/user"
 	"github.com/google/uuid"
 )
 
+// IUserRepository defines methods for accessing and managing user data.
+//
+// Methods:
+// - CreateUser(user *usermodel.User) error: Creates a new user in the repository and
+// returns an error if any occurs.
+// - GetUserById(id uuid.UUID) (*usermodel.User, error): Retrieves a user by their unique
+// ID and returns the user or an error if the user does not exist.
+// - GetUserByUsername(username string) (*usermodel.User, error): Retrieves a user by their
+// username and returns the user or an error if the user does not exist.
+// - ListUser() ([]*usermodel.User, error): Lists all users in the repository and returns
+// a slice of users or an error.
 type IUserRepository interface {
-	CreateUser(user *model.User) error
-	GetUserById(id uuid.UUID) (*model.User, error)
-	GetUserByUsername(id string) (*model.User, error)
-	ListUser() ([]*model.User, error)
+	// CreateUser adds a new user to the repository.
+	CreateUser(user *usermodel.User) error
+
+	// GetUserById retrieves a user by their unique identifier.
+	GetUserById(id uuid.UUID) (*usermodel.User, error)
+
+	// GetUserByUsername retrieves a user by their username.
+	GetUserByUsername(username string) (*usermodel.User, error)
+
+	// ListUser returns a list of all users in the repository.
+	ListUser() ([]*usermodel.User, error)
 }
+
