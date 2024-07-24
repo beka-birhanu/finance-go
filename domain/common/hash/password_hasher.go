6170fa7947a5ba8df7a53d8575165921bc49acae
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 00:27:09 +0300

    doc(domain-hash): add godoc for password_hash interface, refs #14

--- domain/common/hash/password_hasher.go
+++ domain/common/hash/password_hasher.go
@@ -1,7 +1,17 @@
+/*
+Package hash defines an interface for hashing and matching strings.
+*/
 package hash
 
 type IHashService interface {
+	// Hash hashes the provided plain text string and returns the hashed value or
+	// An error if the hashing process fails.
 	Hash(word string) (string, error)
+
+	// Match checks if the hashed string matches the plain text string.
+	// Returns:
+	// - true if the hashed string matches the plain text string.
+	// - false if they do not match.
+	// - An error if the comparison process fails.
 	Match(hashedWord, plainWord string) (bool, error)
 }
-
