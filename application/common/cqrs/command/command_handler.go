1c2ef3810db8e23ded555409a825a960b39273df
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 00:34:25 +0300

    docs(app-cqrs): add godoc for IComandHandler, refs #14

--- application/common/cqrs/command/command_handler.go
+++ application/common/cqrs/command/command_handler.go
@@ -1,5 +1,17 @@
+/*
+Package command provides a generic interface for handling commands.
+
+It includes the `ICommandHandler` interface for processing commands of any type and returning results of any type.
+*/
 package command
 
+// ICommandHandler defines a generic interface for handling commands.
+//
+// Type Parameters:
+// - Command: The type of the command to be handled.
+// - Result: The type of the result returned after handling the command.
 type ICommandHandler[Command any, Result any] interface {
+
+	// Handle processes the provided command and returns the result or an error.
 	Handle(command Command) (Result, error)
 }
