e29370f5e77a2282ec2e94a690b9bd3cd0fe8767
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 23:19:14 +0300

    refactor(app-cqrs): remove "command" prefix for IHandler, refs #14

--- application/common/cqrs/command/command_handler.go
+++ application/common/cqrs/command/command_handler.go
@@ -1,16 +1,16 @@
 /*
 Package command provides a generic interface for handling commands.
 
-It includes the `ICommandHandler` interface for processing commands of any type and returning results of any type.
+It includes the `IHandler` interface for processing commands of any type and returning results of any type.
 */
 package icmd
 
-// ICommandHandler defines a generic interface for handling commands.
+// IHandler defines a generic interface for handling commands.
 //
 // Type Parameters:
 // - Command: The type of the command to be handled.
 // - Result: The type of the result returned after handling the command.
-type ICommandHandler[Command any, Result any] interface {
+type IHandler[Command any, Result any] interface {
 
 	// Handle processes the provided command and returns the result or an error.
 	Handle(command Command) (Result, error)
