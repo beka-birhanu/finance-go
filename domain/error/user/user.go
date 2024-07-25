1afeb0c8725189f1ee5328914c50dec146797b91
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 10:26:52 +0300

    feat(domian-err): consume factory methods of Error in erruser, refs #14

--- domain/error/user/user.go
+++ domain/error/user/user.go
@@ -12,35 +12,35 @@ import "github.com/beka-birhanu/finance-go/domain/error/common"
 // Validation errors
 var (
 	// Username is shorter than allowed.
-	UsernameTooShort = errdmn.New(errdmn.Validation, "username is too short.")
+	UsernameTooShort = errdmn.NewValidation("username is too short.")
 
 	// Username is longer than allowed.
-	UsernameTooLong = errdmn.New(errdmn.Validation, "username is too long.")
+	UsernameTooLong = errdmn.NewValidation("username is too long.")
 
 	// Password is susceptible to attack.
-	WeakPassword = errdmn.New(errdmn.Validation, "password is too weak.")
+	WeakPassword = errdmn.NewValidation("password is too weak.")
 
 	// Username is not UUID.
-	UsernameInvalidFormat = errdmn.New(errdmn.Validation, "username has an invalid format.")
+	UsernameInvalidFormat = errdmn.NewValidation("username has an invalid format.")
 )
 
 // Conflict errors
 var (
 	// User with a similar username exists.
-	UsernameConflict = errdmn.New(errdmn.Conflict, "username already taken.")
+	UsernameConflict = errdmn.NewConflict("username already taken.")
 
 	// ID under user and expense do not match.
-	ExpenseIdConflict = errdmn.New(errdmn.Conflict, "ID under user and expense do not match.")
+	ExpenseIdConflict = errdmn.NewConflict("ID under user and expense do not match.")
 )
 
 // NotFound errors
 var (
 	// User is does not exist.
-	NotFound = errdmn.New(errdmn.NotFound, "User not found.")
+	NotFound = errdmn.NewNotFound("User not found.")
 )
 
 // Unexpected errors
 var (
 	// Unexpected error occurred while hashing.
-	Hash = errdmn.New(errdmn.Unexpected, "error hashing password.")
+	Hash = errdmn.NewUnexpected("error hashing password.")
 )
