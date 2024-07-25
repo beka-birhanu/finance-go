5a59e56d70f94857b07e04ae8215bc9a6ed2c589
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 10:32:50 +0300

    feat(app-err): implement IErr in apperr, refs #14

--- application/error/error.go
+++ application/error/error.go
@@ -7,31 +7,36 @@ package apperror
 
 import (
 	"fmt"
-)
 
-// ErrorType represents the type of an error.
-type ErrorType string
+	ierr "github.com/beka-birhanu/finance-go/domain/common/error"
+)
 
 // Predefined error types.
 const (
 	// AuthenticationErrorType is used for authentication-related errors.
-	Authentication ErrorType = "Authentication"
+	Authentication = "Authentication"
 )
 
 // Error represents a combined application error with a type and message.
 type Error struct {
-	Type    ErrorType
+	kind    string
 	Message string
 }
 
+var _ ierr.IErr = Error{} // Making sure Error implements IErr
+
 // Error formats the Error as a string.
 func (e Error) Error() string {
-	return fmt.Sprintf("%s: %s", e.Type, e.Message)
+	return fmt.Sprintf("%s: %s", e.kind, e.Message)
+}
+
+// Type returns the type of the Error.
+func (e Error) Type() string {
+	return e.kind
 }
 
 // Predefined errors for common scenarios.
 var (
 	// InvalidCredential represents an authentication error due to invalid credentials.
-	InvalidCredential = Error{Type: Authentication, Message: "invalid credentials"}
+	InvalidCredential = Error{kind: Authentication, Message: "invalid credentials"}
 )
-
