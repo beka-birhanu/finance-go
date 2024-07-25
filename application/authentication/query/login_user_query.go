039bd05d6555e63b8a4a2070d777fb7622974679
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 23:26:48 +0300

    refactor(app-auth): remove "UserLogin" prefixfrom loginqry.Query refs #14

--- application/authentication/query/login_user_query.go
+++ application/authentication/query/login_user_query.go
@@ -1,10 +1,17 @@
-package query
+// Package loginqry provides functionality for handling login queries.
+package loginqry
 
-type UserLoginQuery struct {
+// Query represents a login query containing a username and password.
+type Query struct {
+	// Username is the username provided for the login attempt.
 	Username string
+
+	// Password is the password provided for the login attempt.
 	Password string
 }
 
-func NewUserLoginQuery(username, password string) *UserLoginQuery {
-	return &UserLoginQuery{Username: username, Password: password}
+// NewQuery creates and return a new Query instance with the provided
+// username and password.
+func NewQuery(username, password string) *Query {
+	return &Query{Username: username, Password: password}
 }
