60eb7f8c85b2ccf1fdf7b64e9210f27c4c36057a
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 23:20:00 +0300

    refactor(app-cqrs): remove "Query" prefix for IHandler, refs #14

--- application/common/cqrs/query/query_handler.go
+++ application/common/cqrs/query/query_handler.go
@@ -1,16 +1,16 @@
 /*
 Package query provides a generic interface for handling queries.
 
-It includes the `IQueryHandler` interface for processing queries of any type and returning results of any type.
+It includes the `IHandler` interface for processing queries of any type and returning results of any type.
 */
 package iquery
 
-// IQueryHandler defines a generic interface for handling queries.
+// IHandler defines a generic interface for handling queries.
 //
 // Type Parameters:
 // - Query: The type of the query to be handled.
 // - Result: The type of the result returned after handling the query.
-type IQueryHandler[Query any, Result any] interface {
+type IHandler[Query any, Result any] interface {
 
 	// Handle processes the provided query and returns the result or an error.
 	Handle(query Query) (Result, error)
