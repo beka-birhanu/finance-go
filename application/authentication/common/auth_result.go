e1a6e35c600027a26075a2b0f75c4b0de4366d5f
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-25 21:14:30 +0300

    docs(app-auth): add godoc for authResult, refs #14

--- application/authentication/common/auth_result.go
+++ application/authentication/common/auth_result.go
@@ -1,17 +1,26 @@
-package common
+package auth
 
 import "github.com/google/uuid"
 
+// AuthResult represents the result of a successful authentication.
 type AuthResult struct {
-	ID       uuid.UUID
+	// ID is the unique identifier of the authenticated user.
+	ID uuid.UUID
+
+	// Username is the username of the authenticated user.
 	Username string
-	Token    string
+
+	// Token is the authentication token issued to the authenticated user.
+	Token string
 }
 
-func NewAuthResult(id uuid.UUID, username, token string) *AuthResult {
+// Result creates and return a new AuthResult instance with the provided
+// user ID, username, and token.
+func Result(id uuid.UUID, username, token string) *AuthResult {
 	return &AuthResult{
 		ID:       id,
 		Username: username,
 		Token:    token,
 	}
 }
+
