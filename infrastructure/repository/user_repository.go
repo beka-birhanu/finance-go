5050b2fecd78be396c200fb2b985137161a1edc5
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-26 17:57:59 +0300

    docs(infrast-repo): add godoc for methods in user repo, refs #14

--- infrastructure/repository/user_repository.go
+++ infrastructure/repository/user_repository.go
@@ -4,61 +4,81 @@ import (
 	"database/sql"
 
 	"github.com/beka-birhanu/finance-go/application/common/interface/repository"
-	appError "github.com/beka-birhanu/finance-go/application/error"
-	"github.com/beka-birhanu/finance-go/domain/model"
+	erruser "github.com/beka-birhanu/finance-go/domain/error/user"
+	"github.com/beka-birhanu/finance-go/domain/model/user"
 	"github.com/google/uuid"
 )
 
+// UserRepository handles the persistence of user models.
 type UserRepository struct {
 	DB *sql.DB
 }
 
-var users = map[uuid.UUID]model.User{}
+var users = map[uuid.UUID]usermodel.User{} // In-memory storage for users
 
-// Ensure UserRepository implements interfaces.persistance.IUserRepository
-var _ repository.IUserRepository = &UserRepository{}
+// Ensure UserRepository implements repository.IUserRepository.
+var _ irepository.IUserRepository = &UserRepository{}
 
+// NewUserRepository creates a new UserRepository with the given database connection.
 func NewUserRepository(db *sql.DB) *UserRepository {
 	return &UserRepository{
 		DB: db,
 	}
 }
 
-func (u *UserRepository) CreateUser(user *model.User) error {
+// Add inserts a new user into the repository.
+// Returns:
+//   - error: An error if the user already exists, otherwise nil.
+func (u *UserRepository) Add(user *usermodel.User) error {
 	for _, existingUser := range users {
 		if existingUser.Username() == user.Username() {
-			return appError.ErrUsernameConflict
+			return erruser.UsernameConflict
 		}
 	}
 
 	users[user.ID()] = *user
-
 	return nil
 }
 
-func (u *UserRepository) GetUserById(id uuid.UUID) (*model.User, error) {
-
+// ById retrieves a user by their ID.
+//
+// Returns:
+//   - *usermodel.User: A pointer to the retrieved user model.
+//   - error: An error if the user is not found, otherwise nil.
+func (u *UserRepository) ById(id uuid.UUID) (*usermodel.User, error) {
 	user, found := users[id]
 	if !found {
-		return nil, appError.ErrUserNotFound
+		return nil, erruser.NotFound
 	}
 
 	return &user, nil
 }
 
-func (u *UserRepository) GetUserByUsername(username string) (*model.User, error) {
+// ByUsername retrieves a user by their username.
+//
+// Returns:
+//   - *usermodel.User: A pointer to the retrieved user model.
+//   - error: An error if the user is not found, otherwise nil.
+func (u *UserRepository) ByUsername(username string) (*usermodel.User, error) {
 	for _, user := range users {
 		if user.Username() == username {
 			return &user, nil
 		}
 	}
-	return nil, appError.ErrUserNotFound
+	return nil, erruser.NotFound
 }
 
-func (u *UserRepository) ListUser() ([]*model.User, error) {
-	var userList []*model.User
-	for _, user := range users {
-		userList = append(userList, &user)
+// Update modifies an existing user in the repository.
+//
+// Returns:
+//   - error: An error if the user is not found, otherwise nil.
+func (u *UserRepository) Update(user *usermodel.User) error {
+	_, err := u.ById(user.ID())
+	if err != nil {
+		return err
 	}
-	return userList, nil
+
+	users[user.ID()] = *user
+	return nil
 }
+
