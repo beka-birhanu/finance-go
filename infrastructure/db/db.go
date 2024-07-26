e582f2cbeac3aeb1dfecbf6f34990a307e95a655
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-26 16:45:55 +0300

    refacotr(infrast-db): move Connect params to config struct, refs #14

--- infrastructure/db/db.go
+++ infrastructure/db/db.go
@@ -14,11 +14,20 @@ var (
 	once     sync.Once
 )
 
+// Config holds the database connection configuration.
+type Config struct {
+	DbUser     string // Username for the database connection
+	DbPassword string // Password for the database connection
+	DbName     string // Name of the database
+	DbHost     string // Host where the database server is located
+	DbPort     string // Port on which the database server is listening
+}
+
 // Connect initializes and returns a singleton database connection
-func Connect(dbUser, dbPassword, dbName, dbHost, dbPort string) *sql.DB {
+func Connect(config Config) *sql.DB {
 	once.Do(func() {
 		var err error
-		connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+		connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.DbUser, config.DbPassword, config.DbHost, config.DbPort, config.DbName)
 		instance, err = sql.Open("postgres", connStr)
 		if err != nil {
 			log.Fatalf("Could not connect to the database: %v", err)
