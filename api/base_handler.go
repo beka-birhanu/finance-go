a7226472558e5baee6479b94d259b0e644c4f312
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-27 11:21:43 +0300

    feat(api): add global error handling for handlers, refs #14

--- api/base_handler.go
+++ api/base_handler.go
@@ -28,9 +28,38 @@ func (h *BaseHandler) ValidatedBody(r *http.Request, s interface{}) error {
 		if validationErrs, ok := err.(validator.ValidationErrors); ok {
 			return errapi.NewBadRequest(fmt.Sprintf("invalid payload: %v", validationErrs))
 		}
-		return errapi.NewServerError("validation failed")
+		return errapi.NewServerError("request body validation failed")
 	}
 
 	return nil
+}
+
+func (h *BaseHandler) Problem(w http.ResponseWriter, err errapi.Error) {
+	// shadow error messages if they are not supposed to be seen.
+	switch err.StatusCode() {
+	case errapi.BadRequest:
+		httputil.RespondError(w, err)
+
+	case errapi.Conflict:
+		httputil.RespondError(w, err)
+
+	case errapi.NotFound:
+		httputil.RespondError(w, err)
+
+	// client should not know what coused the fail
+	case errapi.Authentication:
+		shadowedErr := errapi.NewAuthentication("invalid credentials")
+		httputil.RespondError(w, shadowedErr)
+
+	// client should not know what coused the fail
+	case errapi.ServerError:
+		shadowedErr := errapi.NewServerError("something went wrong")
+		httputil.RespondError(w, shadowedErr)
+
+	default:
+		error := errapi.NewServerError("something went wrong")
+		httputil.RespondError(w, error)
+	}
+
 }
 
