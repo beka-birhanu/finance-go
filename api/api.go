21ba73afac7877d198019026e939a7f73fa8c4d6
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-27 13:00:03 +0300

    fix(api): update apiservice due to user handler update, refs #14

--- api/api.go
+++ api/api.go
@@ -4,42 +4,40 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/beka-birhanu/finance-go/api/expense"
 	"github.com/beka-birhanu/finance-go/api/user"
-	authCommand "github.com/beka-birhanu/finance-go/application/authentication/command"
-	"github.com/beka-birhanu/finance-go/application/authentication/common"
-	authQuery "github.com/beka-birhanu/finance-go/application/authentication/query"
-	handlerInterface "github.com/beka-birhanu/finance-go/application/common/cqrs/command"
-	"github.com/beka-birhanu/finance-go/application/common/interface/repository"
-	expenseCommand "github.com/beka-birhanu/finance-go/application/expense/command"
-	"github.com/beka-birhanu/finance-go/domain/model"
+	registercmd "github.com/beka-birhanu/finance-go/application/authentication/command"
+	auth "github.com/beka-birhanu/finance-go/application/authentication/common"
+	loginqry "github.com/beka-birhanu/finance-go/application/authentication/query"
+	icmd "github.com/beka-birhanu/finance-go/application/common/cqrs/command"
+	iquery "github.com/beka-birhanu/finance-go/application/common/cqrs/query"
+	irepository "github.com/beka-birhanu/finance-go/application/common/interface/repository"
 	"github.com/gorilla/mux"
 )
 
 type APIServer struct {
-	addr                       string
-	userRepository             repository.IUserRepository
-	userRegisterCommandHandler handlerInterface.ICommandHandler[*authCommand.UserRegisterCommand, *common.AuthResult]
-	userLoginQueryHandler      handlerInterface.ICommandHandler[*authQuery.UserLoginQuery, *common.AuthResult]
-	authorizationMiddleware    func(http.Handler) http.Handler
-	addExpenseCommandHandler   handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense]
+	addr                    string
+	userRepository          irepository.IUserRepository
+	userRegisterHandler     icmd.IHandler[*registercmd.Command, *auth.Result]
+	userLoginQueryHandler   iquery.IHandler[*loginqry.Query, *auth.Result]
+	authorizationMiddleware func(http.Handler) http.Handler
+	// addExpenseCommandHandler   handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense]
 }
 
 func NewAPIServer(
 	addr string,
-	userRepository repository.IUserRepository,
-	userRegisterCommandHandler handlerInterface.ICommandHandler[*authCommand.UserRegisterCommand, *common.AuthResult],
-	userLoginQueryHandler handlerInterface.ICommandHandler[*authQuery.UserLoginQuery, *common.AuthResult],
+	userRepository irepository.IUserRepository,
+	userRegisterHandler icmd.IHandler[*registercmd.Command, *auth.Result],
+	userLoginQueryHandler iquery.IHandler[*loginqry.Query, *auth.Result],
 	authorizationMiddleware func(http.Handler) http.Handler,
-	addExpenseCommandHandler handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense],
+	// addExpenseCommandHandler handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense],
 ) *APIServer {
 	return &APIServer{
-		addr:                       addr,
-		userRepository:             userRepository,
-		userRegisterCommandHandler: userRegisterCommandHandler,
-		userLoginQueryHandler:      userLoginQueryHandler,
-		authorizationMiddleware:    authorizationMiddleware,
-		addExpenseCommandHandler:   addExpenseCommandHandler,
+		addr:                    addr,
+		userRepository:          userRepository,
+		userRegisterHandler:     userRegisterHandler,
+		userLoginQueryHandler:   userLoginQueryHandler,
+		authorizationMiddleware: authorizationMiddleware,
+		// addExpenseCommandHandler:   addExpenseCommandHandler,
 	}
 }
 
@@ -54,16 +52,17 @@ func (s *APIServer) Run() error {
 	protectedRouter.Use(s.authorizationMiddleware)
 
 	userHandler := user.NewHandler(
-		s.userRepository,
-		s.userRegisterCommandHandler,
-		s.userLoginQueryHandler,
-	)
+		user.Config{
+			UserRepository:  s.userRepository,
+			RegisterHandler: s.userRegisterHandler,
+			LoginHandler:    s.userLoginQueryHandler,
+		})
 	userHandler.RegisterPublicRoutes(publicRouter)
 	userHandler.RegisterProtectedRoutes(protectedRouter)
 
-	expenseHandler := expense.NewHandler(s.addExpenseCommandHandler)
-	expenseHandler.RegisterPublicRoutes(publicRouter)
-	expenseHandler.RegisterProtectedRoutes(protectedRouter)
+	// expenseHandler := expense.NewHandler(s.addExpenseCommandHandler)
+	// expenseHandler.RegisterPublicRoutes(publicRouter)
+	// expenseHandler.RegisterProtectedRoutes(protectedRouter)
 
 	log.Println("Listening on", s.addr)
 
