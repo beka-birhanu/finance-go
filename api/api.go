103c8821adbfc08a587abb93149d508343d7f70a
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-27 23:20:59 +0300

    refactor(api): move NewService params to separate config, refs #14

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/beka-birhanu/finance-go/api/expense"
 	"github.com/beka-birhanu/finance-go/api/user"
 	registercmd "github.com/beka-birhanu/finance-go/application/authentication/command"
 	auth "github.com/beka-birhanu/finance-go/application/authentication/common"
@@ -11,60 +12,73 @@ import (
 	icmd "github.com/beka-birhanu/finance-go/application/common/cqrs/command"
 	iquery "github.com/beka-birhanu/finance-go/application/common/cqrs/query"
 	irepository "github.com/beka-birhanu/finance-go/application/common/interface/repository"
+	itimeservice "github.com/beka-birhanu/finance-go/application/common/interface/time_service"
+	expensecmd "github.com/beka-birhanu/finance-go/application/expense/command"
+	expensemodel "github.com/beka-birhanu/finance-go/domain/model/expense"
 	"github.com/gorilla/mux"
 )
 
+// APIServer is the struct for managing the HTTP server and its dependencies.
 type APIServer struct {
-	addr                    string
-	userRepository          irepository.IUserRepository
-	userRegisterHandler     icmd.IHandler[*registercmd.Command, *auth.Result]
-	userLoginQueryHandler   iquery.IHandler[*loginqry.Query, *auth.Result]
-	authorizationMiddleware func(http.Handler) http.Handler
-	// addExpenseCommandHandler   handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense]
+	addr                     string
+	userRepository           irepository.IUserRepository
+	userRegisterHandler      icmd.IHandler[*registercmd.Command, *auth.Result]
+	userLoginQueryHandler    iquery.IHandler[*loginqry.Query, *auth.Result]
+	authorizationMiddleware  func(http.Handler) http.Handler
+	addExpenseCommandHandler icmd.IHandler[*expensecmd.AddCommand, *expensemodel.Expense]
+	timeService              itimeservice.IService
 }
 
-func NewAPIServer(
-	addr string,
-	userRepository irepository.IUserRepository,
-	userRegisterHandler icmd.IHandler[*registercmd.Command, *auth.Result],
-	userLoginQueryHandler iquery.IHandler[*loginqry.Query, *auth.Result],
-	authorizationMiddleware func(http.Handler) http.Handler,
-	// addExpenseCommandHandler handlerInterface.ICommandHandler[*expenseCommand.AddExpenseCommand, *model.Expense],
-) *APIServer {
+// Config is the struct for configuring the APIServer.
+type Config struct {
+	Addr                     string
+	UserRepository           irepository.IUserRepository
+	UserRegisterHandler      icmd.IHandler[*registercmd.Command, *auth.Result]
+	UserLoginQueryHandler    iquery.IHandler[*loginqry.Query, *auth.Result]
+	AuthorizationMiddleware  func(http.Handler) http.Handler
+	AddExpenseCommandHandler icmd.IHandler[*expensecmd.AddCommand, *expensemodel.Expense]
+	TimeService              itimeservice.IService
+}
+
+// NewAPIServer creates a new instance of APIServer with the given configuration.
+func NewAPIServer(config Config) *APIServer {
 	return &APIServer{
-		addr:                    addr,
-		userRepository:          userRepository,
-		userRegisterHandler:     userRegisterHandler,
-		userLoginQueryHandler:   userLoginQueryHandler,
-		authorizationMiddleware: authorizationMiddleware,
-		// addExpenseCommandHandler:   addExpenseCommandHandler,
+		addr:                     config.Addr,
+		userRepository:           config.UserRepository,
+		userRegisterHandler:      config.UserRegisterHandler,
+		userLoginQueryHandler:    config.UserLoginQueryHandler,
+		authorizationMiddleware:  config.AuthorizationMiddleware,
+		addExpenseCommandHandler: config.AddExpenseCommandHandler,
+		timeService:              config.TimeService,
 	}
 }
 
+// Run starts the HTTP server and sets up the routes.
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 
-	// Routes that do not require authentication
+	// Public routes that do not require authentication
 	publicRouter := router.PathPrefix("/api/v1/public").Subrouter()
 
-	// Routes that require authentication
+	// Protected routes that require authentication
 	protectedRouter := router.PathPrefix("/api/v1").Subrouter()
 	protectedRouter.Use(s.authorizationMiddleware)
 
-	userHandler := user.NewHandler(
-		user.Config{
-			UserRepository:  s.userRepository,
-			RegisterHandler: s.userRegisterHandler,
-			LoginHandler:    s.userLoginQueryHandler,
-		})
+	// User routes
+	userHandler := user.NewHandler(user.Config{
+		UserRepository:  s.userRepository,
+		RegisterHandler: s.userRegisterHandler,
+		LoginHandler:    s.userLoginQueryHandler,
+	})
 	userHandler.RegisterPublicRoutes(publicRouter)
 	userHandler.RegisterProtectedRoutes(protectedRouter)
 
-	// expenseHandler := expense.NewHandler(s.addExpenseCommandHandler)
-	// expenseHandler.RegisterPublicRoutes(publicRouter)
-	// expenseHandler.RegisterProtectedRoutes(protectedRouter)
+	// Expense routes
+	expenseHandler := expense.NewHandler(s.addExpenseCommandHandler, s.timeService)
+	expenseHandler.RegisterPublicRoutes(publicRouter)
+	expenseHandler.RegisterProtectedRoutes(protectedRouter)
 
 	log.Println("Listening on", s.addr)
-
 	return http.ListenAndServe(s.addr, router)
 }
+
