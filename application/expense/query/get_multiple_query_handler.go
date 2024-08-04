7b709571887854eff08d8e62b86366a94b6fa592
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-04 22:15:15 +0300

    feat(app-expense): update handler to use return paginated result, #21

--- application/expense/query/get_multiple_query_handler.go
+++ application/expense/query/get_multiple_query_handler.go
@@ -5,6 +5,13 @@ import (
 	expensemodel "github.com/beka-birhanu/finance-go/domain/model/expense"
 )
 
+const (
+	defaultLimit = 10
+	minLimit     = 5
+	maxLimit     = 100
+	sortByAmount = "amount"
+)
+
 type GetMultipleHandler struct {
 	expenseRepository irepository.IExpenseRepository
 }
@@ -13,6 +20,38 @@ func NewGetMultipleHandler(expenseRepository irepository.IExpenseRepository) *Ge
 	return &GetMultipleHandler{expenseRepository: expenseRepository}
 }
 
-func (h *GetMultipleHandler) Handle(getMultipleuery *GetMultipleQuery) ([]*expensemodel.Expense, error) {
-	return h.expenseRepository.List(getMultipleuery.UserId)
+func (h *GetMultipleHandler) Handle(query *GetMultipleQuery) ([]*expensemodel.Expense, error) {
+	// Set defaults if not provided
+	limit := defaultLimit
+	if query.Limit > 0 {
+		// Apply max and min limits
+		if query.Limit < minLimit {
+			limit = minLimit
+		} else if query.Limit > maxLimit {
+			limit = maxLimit
+		} else {
+			limit = query.Limit
+		}
+	}
+
+	// Default to time-based pagination if By is not sortByAmount
+	if query.By == sortByAmount {
+		// Handle amount-based pagination
+		return h.expenseRepository.ListByAmount(irepository.ListByAmountParams{
+			UserID:      query.UserID,
+			Limit:       limit,
+			LastSeenID:  query.LastSeenID,
+			LastSeenAmt: query.LastSeenAmt,
+			Ascending:   query.Ascending,
+		})
+	}
+
+	// Handle time-based pagination as default
+	return h.expenseRepository.ListByTime(irepository.ListByTimeParams{
+		UserID:       query.UserID,
+		Limit:        limit,
+		LastSeenID:   query.LastSeenID,
+		LastSeenTime: query.LastSeenTime,
+		Ascending:    query.Ascending,
+	})
 }
