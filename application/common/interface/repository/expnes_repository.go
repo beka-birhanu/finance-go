4b89183043aaac48c22904f32ac59333975a98a6
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-04 22:10:17 +0300

    feat(app-repo): update expenserepo deff for a paginated response, #21
    - Now there are two methods one for sorting based on amount and the
    other based on creation time.

--- application/common/interface/repository/expnes_repository.go
+++ application/common/interface/repository/expnes_repository.go
@@ -1,17 +1,31 @@
 package irepository
 
 import (
+	"time"
+
 	expensemodel "github.com/beka-birhanu/finance-go/domain/model/expense"
 	"github.com/google/uuid"
 )
 
+// ListByTimeParams holds parameters for ListByTime function
+type ListByTimeParams struct {
+	UserID       uuid.UUID
+	Limit        int
+	LastSeenID   *uuid.UUID
+	LastSeenTime *time.Time
+	Ascending    bool
+}
+
+// ListByAmountParams holds parameters for ListByAmount function
+type ListByAmountParams struct {
+	UserID      uuid.UUID
+	Limit       int
+	LastSeenID  *uuid.UUID
+	LastSeenAmt float64
+	Ascending   bool
+}
+
 // IExpenseRepository defines methods for accessing and managing expense data.
-//
-// Methods:
-// - Save(expense *expensemodel.Expense) error: Inserts or updates an expense in the repository and
-// returns an error if any occurs.
-// - ById(id uuid.UUID, userId uuid.UUID) (*expensemodel.Expense, error): Retrieves an expense by its unique
-// ID and returns the expense or an error if the expense does not exist.
 type IExpenseRepository interface {
 	// Save inserts or updates an expense in the repository.
 	Save(expense *expensemodel.Expense) error
@@ -19,6 +33,9 @@ type IExpenseRepository interface {
 	// ById retrieves an expense by its unique identifier and user ID.
 	ById(id uuid.UUID, userId uuid.UUID) (*expensemodel.Expense, error)
 
-	// List retrieves expenses by user ID.
-	List(userId uuid.UUID) ([]*expensemodel.Expense, error)
+	// ListByTime retrieves paginated expenses by user ID based on created time.
+	ListByTime(params ListByTimeParams) ([]*expensemodel.Expense, error)
+
+	// ListByAmount retrieves paginated expenses by user ID based on amount.
+	ListByAmount(params ListByAmountParams) ([]*expensemodel.Expense, error)
 }
