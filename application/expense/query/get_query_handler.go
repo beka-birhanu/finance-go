78fad44731d789d14de023b2619e52991340e0e4
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-06 23:32:13 +0300

    docs(expensqry): add comments to GetHandler for better understanding #22

--- application/expense/query/get_query_handler.go
+++ application/expense/query/get_query_handler.go
@@ -6,16 +6,24 @@ import (
 	expensemodel "github.com/beka-birhanu/finance-go/domain/model/expense"
 )
 
+// GetHandler processes queries to retrieve a specific expense.
 type GetHandler struct {
 	expenseRepository irepository.IExpenseRepository
 }
 
+// Ensure GetHandler implements iquery.IHandler interface for GetQuery.
 var _ iquery.IHandler[*GetQuery, *expensemodel.Expense] = &GetHandler{}
 
+// NewGetHandler creates a new instance of GetHandler with the provided expense repository.
 func NewGetHandler(expenseRepository irepository.IExpenseRepository) *GetHandler {
 	return &GetHandler{expenseRepository: expenseRepository}
 }
 
+// Handle retrieves an expense based on the provided query parameters.
+//
+// Returns:
+//   - *expensemodel.Expense: The retrieved expense if found.
+//   - error: An error if the retrieval fails.
 func (h *GetHandler) Handle(query *GetQuery) (*expensemodel.Expense, error) {
 	return h.expenseRepository.ById(query.ExpenseId, query.UserId)
 }
