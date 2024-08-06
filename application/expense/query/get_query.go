642a468164f0aecfbd09b431e4952e9821a0173c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-06 23:30:51 +0300

    docs(expensqry): add comments to GetQuery for better understanding #22

--- application/expense/query/get_query.go
+++ application/expense/query/get_query.go
@@ -2,7 +2,8 @@ package expensqry
 
 import "github.com/google/uuid"
 
+// GetQuery represents a query for retrieving a specific expense.
 type GetQuery struct {
-	UserId    uuid.UUID
-	ExpenseId uuid.UUID
+	UserId    uuid.UUID // ID of the user
+	ExpenseId uuid.UUID // ID of the expense
 }
