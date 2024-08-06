5b0b913fc811e1b907ad1124c58c5f08528b5135
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-06 23:26:40 +0300

    docs(expensqry): add comments to GetMultipleQuery fields for clarity #22

--- application/expense/query/get_multiple_query.go
+++ application/expense/query/get_multiple_query.go
@@ -6,12 +6,14 @@ import (
 	"github.com/google/uuid"
 )
 
+// GetMultipleQuery represents a query for retrieving multiple expenses.
 type GetMultipleQuery struct {
-	UserID       uuid.UUID
-	Limit        int
-	By           string
-	LastSeenID   *uuid.UUID
-	LastSeenTime *time.Time
-	LastSeenAmt  float64
-	Ascending    bool
+	UserID       uuid.UUID  // ID of the user whose expenses are to be retrieved
+	Limit        int        // Maximum number of expenses to retrieve
+	By           string     // Field to sort by (e.g., "date", "amount")
+	LastSeenID   *uuid.UUID // ID of the last seen expense (for pagination)
+	LastSeenTime *time.Time // Time of the last seen expense (for pagination)
+	LastSeenAmt  float64    // Amount of the last seen expense (for pagination)
+	Ascending    bool       // Whether to sort in ascending order
 }
+
