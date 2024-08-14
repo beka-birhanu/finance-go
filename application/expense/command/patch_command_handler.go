180808dba246c377dc7f777ece9bd0689f2c24ce
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-14 11:26:54 +0300

    docs(app): add godoc for patch_command_handler.go #22

--- application/expense/command/patch_command_handler.go
+++ application/expense/command/patch_command_handler.go
@@ -1,3 +1,4 @@
+// Package expensecmd provides functionality for handling commands related to expenses.
 package expensecmd
 
 import (
@@ -5,27 +6,32 @@ import (
 	expensemodel "github.com/beka-birhanu/finance-go/domain/model/expense"
 )
 
-// PatchHandler handles patching an expense.
+// PatchHandler manages the patching of expenses.
 type PatchHandler struct {
-	expenseRepository irepository.IExpenseRepository
+	expenseRepository irepository.IExpenseRepository // Repository for expense data
 }
 
-// NewPatchHandler creates a new PatchHandler.
+// NewPatchHandler creates a new PatchHandler with the provided expense repository.
 func NewPatchHandler(expenseRepository irepository.IExpenseRepository) *PatchHandler {
 	return &PatchHandler{
 		expenseRepository: expenseRepository,
 	}
 }
 
-// Handle handles the patching of an expense.
+// Handle processes a PatchCommand to update an existing expense.
+// It retrieves the expense by its ID and user ID, updates the expense fields if provided,
+// and saves the changes to the repository.
+//
+// Returns:
+//   - *expensemodel.Expense: The updated expense.
+//   - error: An error if the update fails. Possible errors include issues with retrieving
+//     or updating the expense, or saving the changes to the repository.
 func (h *PatchHandler) Handle(cmd *PatchCommand) (*expensemodel.Expense, error) {
-	// Retrieve the expense by ID and User ID
 	expense, err := h.expenseRepository.ById(cmd.Id, cmd.UserId)
 	if err != nil {
 		return nil, err
 	}
 
-	// Update the expense fields if provided
 	if cmd.Amount != nil {
 		if err := expense.UpdateAmount(*cmd.Amount); err != nil {
 			return nil, err
@@ -40,11 +46,10 @@ func (h *PatchHandler) Handle(cmd *PatchCommand) (*expensemodel.Expense, error) 
 		expense.UpdateDate(*cmd.Date)
 	}
 
-	err = h.expenseRepository.Save(expense)
-	if err != nil {
+	if err := h.expenseRepository.Save(expense); err != nil {
 		return nil, err
 	}
 
-	// TODO: publish an expense udpated domain event
+	// TODO: Publish an expense updated domain event
 	return expense, nil
 }
