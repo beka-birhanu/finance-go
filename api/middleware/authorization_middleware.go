f53408c2d6fa83169ed0faa3b7f1787ea263a873
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-09-21 22:37:32 +0300

    refactor: refactore authorize middleware to accept blockIfInvalid param, #24

--- api/middleware/authorization_middleware.go
+++ api/middleware/authorization_middleware.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	ijwt "github.com/beka-birhanu/finance-go/application/common/interface/jwt"
+	"github.com/dgrijalva/jwt-go"
 )
 
 // contextKey is a type for context keys used in this package.
@@ -18,11 +19,11 @@ const ContextUserClaims contextKey = "userClaims"
 // Authorization is a middleware that validates the JWT token from the request cookie.
 // If the token is valid, the user claims are attached to the request context; otherwise,
 // it returns an HTTP 401 Unauthorized error.
-func Authorization(jwtService ijwt.IService) func(http.Handler) http.Handler {
+func Authorization(jwtService ijwt.IService, blockIfInvalid bool) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cookie, err := r.Cookie("accessToken")
-			if err != nil {
+			claims, err := extractAndDecodeToken(jwtService, r, "accessToken")
+			if err != nil && blockIfInvalid {
 				if errors.Is(err, http.ErrNoCookie) {
 					http.Error(w, "Authorization token required", http.StatusUnauthorized)
 				} else {
@@ -30,16 +31,19 @@ func Authorization(jwtService ijwt.IService) func(http.Handler) http.Handler {
 				}
 				return
 			}
-
-			tokenString := cookie.Value
-			claims, err := jwtService.Decode(tokenString)
-			if err != nil {
-				http.Error(w, "Invalid token", http.StatusUnauthorized)
-				return
-			}
-
 			ctx := context.WithValue(r.Context(), ContextUserClaims, claims)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
+}
+
+func extractAndDecodeToken(jwtService ijwt.IService, r *http.Request, tokenKey string) (jwt.MapClaims, error) {
+	cookie, err := r.Cookie(tokenKey)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenString := cookie.Value
+	claims, err := jwtService.Decode(tokenString)
+	return claims, err
 }
